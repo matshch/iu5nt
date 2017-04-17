dc1664275434621c1719c27e4aedb3f43f3944f5
Konstantin Melnikov <hexagramg@gmail.com>
2017-04-17 19:48:01 +0300

    Kek

---
+++ iu5nt/Kostyan level/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iu5nt.Kostyan_level
+{
+    class DataLink
+    {
+
+    }
+    class Physical
+    {
+
+    }
+}
