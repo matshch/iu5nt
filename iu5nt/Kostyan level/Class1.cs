51bd6311bb81e746a161be9cabb5033b46a71087
Konstantin Melnikov <hexagramg@gmail.com>
2017-04-24 21:06:59 +0300

    Заменил

--- iu5nt/Kostyan level/Class1.cs
+++ iu5nt/Kostyan level/Class1.cs
@@ -170,7 +170,7 @@ namespace iu5nt.Kostyan_level
         public static bool connected = false;
         public static List<UInt32> failList = learning();
         public delegate void PortCheck(bool DSR, bool CTS);
-        public delegate void PortListener(SerialPinChange changed);
+        public delegate void PortListener(bool DSR, bool CTS);
         public static event PortCheck onCheck;
         public static event PortListener UICheck;
         public static void SetRts(bool setter)
@@ -178,7 +178,7 @@ namespace iu5nt.Kostyan_level
             _serialPort.RtsEnable = setter;
         }
         static void StatusCheck (Object sender, SerialPinChangedEventArgs e){
-            UICheck(e.EventType);
+            UICheck(_serialPort.DsrHolding, _serialPort.CtsHolding);
         }
         public static void Connect(String portName)
         {
